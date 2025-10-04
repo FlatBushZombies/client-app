bbd405f8e6472257c52e1a7924ee8cdd1db3d071
FlatBushZombies <brianmakanjira18@gmail.com>
2025-10-04 13:22:49 +0200

    env fix v1.1

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -1,11 +1,8 @@
 import { neon } from "@neondatabase/serverless"
 
-// make sure DATABASE_URL is set in .env.local or Vercel → Project → Settings → Environment Variables
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined in environment variables")
-}
 
-const sql = neon(process.env.DATABASE_URL)
+
+const sql = neon(process.env.DATABASE_URL!)
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
